433a4514811281e4bef5852774490c3d7692777c
unknown <gkundan525@gmail.com>
2020-12-31 20:28:38 +0530

    adding the cart icon and the cart components and the reducer

--- src/components/header/header.componetn.jsx
+++ src/components/header/header.componetn.jsx
@@ -6,8 +6,12 @@ import "./header.componetnts.style.scss";
 import { ReactComponent as Logo } from "../../assets/online-shop.svg";
 //import auth for the user if signIn then show signOut Button on the NavBar
 import { auth } from "../../firebase/firebase.utils";
+///import cart
+import CartIcon from "../cart-icon/cart-icon.component";
+//cart dropdown
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link to="/" className="logo-container">
       <Logo className="logo" />
@@ -28,14 +32,17 @@ const Header = ({ currentUser }) => (
           SIGNIN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
 //we got the state value in const
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
